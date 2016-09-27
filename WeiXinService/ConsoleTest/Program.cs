78cb9f60ec2dad3c9b1ad6040a813b68fb46eaed
ASER1989 <aser1989@sina.com>
2016-09-27 10:20:55 +0800

    ii

---
+++ WeiXinService/ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new TimeSpan(0, 300, 0);
+            Console.Write(date);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WeiXinService/Wx.weixin/Api.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Wx.weixin
+{
+    public class Api:BaseApi
+    {
+        public override void GetAuth()
+        {
+            //WebRequest req = new WebRequest();
+            //req.GetResponse();
+        }
+
+        public override void GetToken()
+        {
+            //throw new NotImplementedException();
+        }
+
+    }
+}
