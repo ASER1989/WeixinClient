905220e2747f341cb0bc87f164b14d8bf356a7ee
ASER1989 <aser1989@sina.com>
2016-11-15 17:32:04 +0800

    ii

--- WeiXinService/Wx.Client/Controllers/TestController.cs
+++ WeiXinService/Wx.Client/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Wx.Extend;
 
 namespace Wx.Client.Controllers
@@ -14,10 +15,66 @@ namespace Wx.Client.Controllers
 
         public ActionResult QrTest()
         {
-            string fileName = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss:fff").ToMd5()+".jpg";
-            new Qrcode().SaveQrcode("http://wx.ahyunhe.com/home/RedPackTest", Server.MapPath("/Res/Links") + "/"+fileName);
+            string fileName = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss:fff").ToMd5() + ".jpg";
+            new Qrcode().SaveQrcode("http://wx.ahyunhe.com/home/RedPackTest", Server.MapPath("/Res/Links") + "/" + fileName);
             TempData["img"] = "/Res/Links/" + fileName;
             return View();
+        }
+
+        public ActionResult index(string pwd)
+        {
+            if (pwd == "今天天气不错！")
+            {
+                SessionCore.Set("_key_Password",pwd);
+                return Redirect("/test/config");
+            }
+            return View();
+        }
+        public ActionResult Config()
+        {
+            if (SessionCore.Get("_key_Password").ObjToString() != "今天天气不错！") {
+                return Redirect("/test/index");
+            }
+
+            string filepath = Server.MapPath("/Res/Data/whitelist.txt");
+            string txt = Cache.CacheApi.Get("_white_list").ObjToString() ?? System.IO.File.ReadAllText(filepath);
+            TempData["data"] = txt;
+            return View();
+        }
+        public ActionResult SetConfig(string openid, string sub)
+        {
+            if (SessionCore.Get("_key_Password").ObjToString() != "今天天气不错！")
+            {
+                return Redirect("/test/index");
+            }
+
+            string filepath = Server.MapPath("/Res/Data/whitelist.txt");
+            var txt = System.IO.File.ReadAllText(filepath);
+            var js = new JavaScriptSerializer();
+            var data = js.Deserialize<List<string>>(txt);
+
+            if (sub == "添加")
+            {
+                data.Add(openid);
+            }
+            else
+            {
+                data.Remove(openid);
+            }
+
+            txt = js.Serialize(data);
+            System.IO.File.WriteAllText(filepath, txt);
+            Cache.CacheApi.Remove("_white_list");
+            return Redirect("/test/config");
+        }
+        public string Clean()
+        {
+            Cache.CacheApi.Remove("_white_list");
+
+            string filepath = Server.MapPath("/Res/Data/whitelist.txt");
+            string txt = System.IO.File.ReadAllText(filepath);
+            Cache.CacheApi.Set("_white_list", txt);
+            return Cache.CacheApi.Get("_white_list").ObjToString();
         }
 
     }
