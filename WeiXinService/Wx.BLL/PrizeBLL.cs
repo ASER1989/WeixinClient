fcad7e661b632cb56570a4fe0c5d9ab8535135cc
ASER\ASER_MANAGER <aser1989@sina.com>
2016-12-31 20:06:00 +0800

    iii

---
+++ WeiXinService/Wx.BLL/PrizeBLL.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+
+namespace Wx.BLL
+{
+    public class PrizeBLL
+    {
+        public PrizeCode GetPrizeByCode(string code)
+        {
+            using (var edm = new DataModel())
+            {
+                var data = edm.PrizeCode.FirstOrDefault(p => p.BarCode == code);
+                return data;
+            }
+        }
+
+        public PrizeSettings GetPrizeSetting(string no) {
+            using (var edm = new DataModel())
+            {
+                var data = edm.PrizeSettings.FirstOrDefault(p => p.PrizeNo == no);
+                return data;
+            }
+        }
+
+
+        /// <summary>
+        /// 修改奖品领奖状态
+        /// </summary>
+        /// <param name="code"></param>
+        public void UpdatePrizeState(string code)
+        {
+            using (var edm = new DataModel())
+            {
+                var data = edm.PrizeCode.FirstOrDefault(p => p.BarCode == code);
+                if (data != null)
+                {
+                    data.AwardFlag = true;
+                    var setting = edm.PrizeSettings.FirstOrDefault(p => p.PrizeNo == data.PrizeNo);
+                    if (setting != null)
+                    {
+                        setting.WinningCount += 1;
+
+                    }
+                    edm.SaveChanges();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 奖品领奖状态回滚
+        /// </summary>
+        /// <param name="code"></param>
+        public void PrizeStateReback(string code) {
+            using (var edm = new DataModel())
+            {
+                var data = edm.PrizeCode.FirstOrDefault(p => p.BarCode == code);
+                if (data != null)
+                {
+                    data.AwardFlag =false;
+                    var setting = edm.PrizeSettings.FirstOrDefault(p => p.PrizeNo == data.PrizeNo);
+                    if (setting != null)
+                    {
+                        setting.WinningCount -= 1;
+                        
+                    }
+                    edm.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ WeiXinService/Wx.Client/Controllers/PrizeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wx.BLL;
+using Wx.Client.Filter;
+using Wx.Client.Models;
+using Wx.Extend;
+using Wx.Weixin;
+
+namespace Wx.Client.Controllers
+{
+    public class PrizeController : Controller
+    {
+        //
+        // GET: /Prize/
+        [Login]
+        public ActionResult Index(string code = null)
+        {
+            var prbll = new PrizeBLL();
+            var data = prbll.GetPrizeByCode(code);
+
+            TempData["isRedPack"] = "0";
+            if (data!=null)
+            {
+                 var prize = prbll.GetPrizeSetting(data.PrizeNo);
+                //prizeType：虚拟奖品1，实物奖品2
+                 if (prize != null && prize.PrizeType == 1)
+                 {
+                     TempData["isRedPack"] = "1";
+                 }
+            }
+            return View(data);
+        }
+
+        [Login]
+        public ActionResult Detail(string code = null)
+        {
+            var prbll = new PrizeBLL();
+            var data = prbll.GetPrizeByCode(code);
+            TempData["isWin"] = "-1";
+            //发红包
+            if (data != null  && data.AwardFlag == false)
+            {
+                TempData["isWin"] = "0";
+                var prize = prbll.GetPrizeSetting(data.PrizeNo);
+                //prizeType：虚拟奖品1，实物奖品2
+                if (prize != null && prize.PrizeType==1) {
+                    //修改奖品状态
+                    prbll.UpdatePrizeState(data.BarCode);
+
+                    var res = new RedPack().SendReadPack(SessionCore.OpenId, prize.Money ?? 0);
+                    if (res.return_code == "SUCCESS" && res.result_code == "SUCCESS")
+                    {
+                        return View(data);
+                    }
+                    else
+                    {
+                        //奖品状态回滚
+                        prbll.PrizeStateReback(data.BarCode);
+                        return Redirect("/res/error/Error.html");
+                    }
+                }
+                
+            }
+            return View(data);
+        }
+
+    }
+}
